f912e99d38b07f6e8c5baba3f59feba9317c9f88
Florian Schulze <florian.schulze@mpi-hd.mpg.de>
2025-06-19 02:00:39 +0200

    feat(plot): Add plot_render_vew_2d

--- src/sapphireppplot/plot.py
+++ src/sapphireppplot/plot.py
@@ -1,6 +1,7 @@
 """Show plots using ParaView"""
 
 from typing import Optional
+import os
 import paraview.simple as ps
 import paraview.servermanager
 from sapphireppplot.plot_properties import PlotProperties
@@ -8,7 +9,7 @@ from sapphireppplot.plot_properties import PlotProperties
 
 def plot_line_chart_view(
     solution: paraview.servermanager.SourceProxy,
-    layout_name: str = "Layout #1",
+    layout_name: str = "LineChartView",
     title: str = "",
     visible_lines: Optional[list[str]] = None,
     plot_properties: PlotProperties = PlotProperties(),
@@ -57,6 +58,8 @@ def plot_line_chart_view(
     # assign view to a particular cell in the layout
     ps.AssignViewToLayout(view=line_chart_view, layout=layout, hint=0)
 
+    # set active view
+    ps.SetActiveView(line_chart_view)
     # set active source
     ps.SetActiveSource(solution)
 
@@ -79,8 +82,166 @@ def plot_line_chart_view(
     return layout, line_chart_view
 
 
+def plot_render_view_2d(
+    solution: paraview.servermanager.SourceProxy,
+    quantity: str,
+    layout_name: str = "2D RenderView",
+    legend_title: str = "",
+    value_range: Optional[list[float]] = None,
+    plot_properties: PlotProperties = PlotProperties(),
+) -> tuple[
+    paraview.servermanager.ViewLayoutProxy, paraview.servermanager.Proxy
+]:
+    """
+    Creates and configures a 2D render view
+    for visualizing data from a given solution in ParaView.
+
+    Parameters
+    ----------
+    solution : paraview.servermanager.SourceProxy
+        The data source to visualize, typically a ParaView data object.
+    quantity : str
+        Name of the quantity to plot.
+    layout_name : str, optional
+        Name of the layout to create for the view.
+    legend_title : str, optional
+        Title for the color bar legend.
+    value_range : list[float], optional
+        Minimal (`value_range[0]`)
+        and maximal (`value_range[1]`) value for the color bar.
+    plot_properties : PlotProperties, optional
+        Properties for plotting like the labels.
+
+    Returns
+    -------
+    layout : paraview.servermanager.LayoutProxy
+        The layout object containing the configured render view.
+    render_view : paraview.servermanager.RenderViewProxy
+        The configured 2D render view.
+    """
+
+    # create new layout object
+    layout = ps.CreateLayout(name=layout_name)
+
+    # Create a new 'Line Chart View'
+    render_view = ps.CreateView("RenderView")
+
+    # assign view to a particular cell in the layout
+    ps.AssignViewToLayout(view=render_view, layout=layout, hint=0)
+
+    # set active view
+    ps.SetActiveView(render_view)
+    # set active source
+    ps.SetActiveSource(solution)
+
+    # show data in view
+    solution_display = ps.Show(
+        solution, render_view, "UnstructuredGridRepresentation"
+    )
+    # Enter preview mode
+    layout.PreviewMode = [1024, 1024]
+    # layout/tab size in pixels
+    layout.SetSize(1024, 1024)
+
+    # update the view to ensure updated data information
+    render_view.Update()
+
+    # reset view to fit data
+    render_view.ResetCamera(False, 0.9)
+
+    # Hide orientation axes
+    render_view.OrientationAxesVisibility = 0
+
+    # changing interaction mode based on data extents
+    render_view.InteractionMode = "2D"
+
+    # Properties modified on render_view
+    render_view.UseColorPaletteForBackground = 0
+    render_view.BackgroundColorMode = "Single Color"
+    render_view.Background = [1.0, 1.0, 1.0]
+
+    # Properties modified on solution_display
+    solution_display.DisableLighting = 1
+    solution_display.Diffuse = 1.0
+
+    # ------------------
+    # Show DataAxesGrid
+    # ------------------
+
+    solution_display.DataAxesGrid.GridAxesVisibility = 1
+    solution_display.DataAxesGrid.XTitle = "$x$"
+    solution_display.DataAxesGrid.YTitle = "$y$  "
+    # Only show Axes Min-X//Y/Z
+    solution_display.DataAxesGrid.AxesToLabel = 7
+    # solution_display.DataAxesGrid.FacesToRender = 7
+    # Set default font size: 24 for title, 18 for label
+    solution_display.DataAxesGrid.XTitleFontSize = 24
+    solution_display.DataAxesGrid.YTitleFontSize = 24
+    solution_display.DataAxesGrid.XLabelFontSize = 18
+    solution_display.DataAxesGrid.YLabelFontSize = 18
+    # Use gray color for label for good visibility in both light and dark mode
+    solution_display.DataAxesGrid.XTitleColor = [0.5, 0.5, 0.5]
+    solution_display.DataAxesGrid.YTitleColor = [0.5, 0.5, 0.5]
+    solution_display.DataAxesGrid.XLabelColor = [0.5, 0.5, 0.5]
+    solution_display.DataAxesGrid.YLabelColor = [0.5, 0.5, 0.5]
+    solution_display.DataAxesGrid.GridColor = [0.5, 0.5, 0.5]
+
+    # update the view to ensure updated data information
+    render_view.Update()
+
+    # ----------------------
+    # Create color bar plot
+    # ----------------------
+
+    # set scalar coloring
+    ps.ColorBy(solution_display, ("POINTS", quantity))
+
+    # show color bar/color legend
+    solution_display.SetScalarBarVisibility(render_view, True)
+
+    # get color transfer function/color map for 'rho'
+    transfer_color = ps.GetColorTransferFunction(quantity)
+
+    # get opacity transfer function/opacity map for 'rho'
+    transfer_opacity = ps.GetOpacityTransferFunction(quantity)
+
+    # get 2D transfer function for 'rho'
+    transfer_function = ps.GetTransferFunction2D(quantity)
+
+    # Rescale transfer function
+    if value_range:
+        transfer_color.RescaleTransferFunction(value_range[0], value_range[1])
+        transfer_opacity.RescaleTransferFunction(value_range[0], value_range[1])
+        transfer_function.RescaleTransferFunction(
+            value_range[0], value_range[1], 0.0, 1.0
+        )
+
+    # Apply a preset using its name. Note this may not work as expected when presets have duplicate names.
+    transfer_color.ApplyPreset("Viridis (matplotlib)", True)
+    # transfer_color.ApplyPreset("Cool to Warm", True)
+
+    # get color legend/bar for f_000LUT in view render_view
+    color_bar = ps.GetScalarBar(transfer_color, render_view)
+
+    # Properties modified on color_bar
+    color_bar.Title = legend_title
+    color_bar.TitleFontSize = 24
+    color_bar.LabelFontSize = 18
+    color_bar.TitleColor = [0.5, 0.5, 0.5]
+    color_bar.LabelColor = [0.5, 0.5, 0.5]
+    # color_bar.TitleColor = [1.0, 1.0, 1.0]
+    # color_bar.LabelColor = [1.0, 1.0, 1.0]
+
+    # change scalar bar placement
+    color_bar.WindowLocation = "Any Location"
+    color_bar.ScalarBarLength = 0.25
+    color_bar.Position = [0.65, 0.1]
+
+    return layout, render_view
+
+
 def save_screenshot(
-    line_chart_view: paraview.servermanager.ViewLayoutProxy,
+    view_or_layout: paraview.servermanager.ViewLayoutProxy,
     results_folder: str,
     filename: str,
 ):
@@ -89,24 +250,25 @@ def save_screenshot(
 
     Parameters
     ----------
-    line_chart_view : paraview.servermanager.ViewLayoutProxy
+    view_or_layout : paraview.servermanager.ViewLayoutProxy
         The ParaView view or layout object to capture in the screenshot.
     results_folder : str
         The directory path where the screenshot will be saved.
     filename : str
         The base name for the screenshot file (without extension).
     """
-    print(f"Save screenshot '{results_folder}/{filename}.png'")
+    file_path = os.path.join(results_folder, filename + ".png")
+    print(f"Save screenshot '{file_path}")
     ps.SaveScreenshot(
-        filename=results_folder + "/" + filename + ".png",
-        viewOrLayout=line_chart_view,
+        filename=file_path,
+        viewOrLayout=view_or_layout,
         location=ps.vtkPVSession.DATA_SERVER,
         TransparentBackground=1,
     )
 
 
 def save_animation(
-    line_chart_view: paraview.servermanager.ViewLayoutProxy,
+    view_or_layout: paraview.servermanager.ViewLayoutProxy,
     results_folder: str,
     filename: str,
 ):
@@ -115,17 +277,18 @@ def save_animation(
 
     Parameters
     ----------
-    line_chart_view : paraview.servermanager.ViewLayoutProxy
+    view_or_layout : paraview.servermanager.ViewLayoutProxy
         The ParaView view or layout object to capture in the screenshot.
     results_folder : str
         The directory path where the screenshot will be saved.
     filename : str
         The base name for the screenshot file (without extension).
     """
-    print(f"Save animation '{results_folder}/{filename}.*.png'")
+    file_path = os.path.join(results_folder, filename + ".png")
+    print(f"Save animation '{file_path}")
     ps.SaveAnimation(
-        filename=results_folder + "/" + filename + ".png",
-        viewOrLayout=line_chart_view,
+        filename=file_path,
+        viewOrLayout=view_or_layout,
         location=ps.vtkPVSession.DATA_SERVER,
         TransparentBackground=1,
         FrameStride=1,
