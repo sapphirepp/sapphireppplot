923a74deb82db3288ebb5745f8c5a4f0bb2460aa
Florian Schulze <florian.schulze@mpi-hd.mpg.de>
2025-08-27 15:53:27 +0200

    fix(utils): Use normpath
    Add words to spell list

--- src/sapphireppplot/utils.py
+++ src/sapphireppplot/utils.py
@@ -56,6 +56,7 @@ def get_results_folder(
     results_folder = os.path.expanduser(results_folder)
     if path_prefix and not os.path.isabs(results_folder):
         results_folder = os.path.join(path_prefix, results_folder)
+    results_folder = os.path.normpath(results_folder)
 
     print(f"Using results in '{results_folder}'")
     return results_folder
