f8c0fdebda245f4cc08095c283c5826edf29558d
Florian Schulze <florian.schulze@mpi-hd.mpg.de>
2025-11-15 19:41:25 +0100

    feat(numpyify): Add to_numpy_time_steps_2d/3d

--- src/sapphireppplot/numpyify.py
+++ src/sapphireppplot/numpyify.py
@@ -212,7 +212,7 @@ def to_numpy_3d(
     """
     points, data = to_numpy_point_list(solution, array_names)
 
-    # Reshape arrays to 2D arrays
+    # Reshape arrays to 3D arrays
     indices_x = np.argwhere(
         (points[:, 1] == points[0, 1]) & (points[:, 2] == points[0, 2])
     )  # y,z constant
@@ -263,7 +263,7 @@ def to_numpy_time_steps(
         ``points[t][i] = [x, y, z]``.
     data : list[np.ndarray]
         List of 2D arrays ``data[t][c][i]`` with the data from the solution.
-        The first index corresponds to ``time_step[t]``,
+        The first index corresponds to ``time_steps[t]``,
         the second index ``c`` to ``array_names[c]``,
         the third index to the point ``points[t][i]``.
 
@@ -326,3 +326,133 @@ def to_numpy_time_steps(
         data_array[i] = data
 
     return time_steps, points_array, data_array
+
+
+def to_numpy_time_steps_2d(
+    solution: paraview.servermanager.SourceProxy,
+    array_names: list[str],
+    time_steps: Optional[list[float]] = None,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Convert time dependent 2D data to a numpy array with the data evaluated at the cell centers.
+
+    Assumes that the grid is regular and is constant over time.
+
+    Parameters
+    ----------
+    solution
+        ParaView solution data.
+    array_names
+        List of array names that should be extracted.
+    time_steps
+        List of time steps to extract the data.
+        Defaults to using all time steps.
+
+    Returns
+    -------
+    time_steps : np.ndarray
+        The time steps:
+        ``time_steps[t] = time``
+        where ``t`` is the index of the time step.
+    points : np.ndarray
+        The x/y/z-values of the points organized in a 2D grid:
+        ``points[i][j] = [x, y, z]``.
+        Sorted so that ``x`` corresponds to ``i`` and ``y`` to ``j``.
+    data : np.ndarray
+        4D array ``data[t][c][i][j]`` with the data from the solution.
+        The first index ``t`` corresponds to ``time_steps[t]``,
+        the second index ``c`` to ``array_names[c]``,
+        the third and forth index corresponds to ``points[i][j]``.
+
+    See Also
+    --------
+    to_numpy_time_steps : Get numpy arrays of data as point list for multiple time steps.
+    """
+    time_steps_out, points, data = to_numpy_time_steps(
+        solution, array_names, time_steps=time_steps
+    )
+
+    time_steps_out = np.array(time_steps_out)
+    points = points[0]
+    data = np.array(data)
+
+    # Reshape arrays to numpy arrays
+    indices_x = np.argwhere(points[:, 1] == points[0, 1])  # y constant
+    indices_y = np.argwhere(points[:, 0] == points[0, 0])  # x constant
+    size_x = indices_x.shape[0]
+    size_y = indices_y.shape[0]
+    points = points.reshape((size_x, size_y, 3))
+    data = data.reshape((time_steps_out.size, len(array_names), size_x, size_y))
+
+    return time_steps_out, points, data
+
+
+def to_numpy_time_steps_3d(
+    solution: paraview.servermanager.SourceProxy,
+    array_names: list[str],
+    time_steps: Optional[list[float]] = None,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Convert time dependent 3D data to a numpy array with the data evaluated at the cell centers.
+
+    Assumes that the grid is regular and is constant over time.
+
+    Parameters
+    ----------
+    solution
+        ParaView solution data.
+    array_names
+        List of array names that should be extracted.
+    time_steps
+        List of time steps to extract the data.
+        Defaults to using all time steps.
+
+    Returns
+    -------
+    time_steps : np.ndarray
+        The time steps:
+        ``time_steps[t] = time``
+        where ``t`` is the index of the time step.
+    points : np.ndarray
+        The x/y/z-values of the points organized in a 3D grid:
+        ``points[i][j][k] = [x, y, z]``.
+        Sorted so that ``x`` corresponds to ``i``,
+        ``y`` to ``j``
+        and ``z`` to ``k``.
+    data : np.ndarray
+        5D array ``data[t][c][i][j][k]`` with the data from the solution.
+        The first index ``t`` corresponds to ``time_steps[t]``,
+        the second index ``c`` to ``array_names[c]``,
+        the third, forth and fifth index corresponds to ``points[i][j][k]``.
+
+    See Also
+    --------
+    to_numpy_time_steps : Get numpy arrays of data as point list for multiple time steps.
+    """
+    time_steps_out, points, data = to_numpy_time_steps(
+        solution, array_names, time_steps=time_steps
+    )
+
+    time_steps_out = np.array(time_steps_out)
+    points = points[0]
+    data = np.array(data)
+
+    # Reshape arrays to numpy arrays
+    indices_x = np.argwhere(
+        (points[:, 1] == points[0, 1]) & (points[:, 2] == points[0, 2])
+    )  # y,z constant
+    indices_y = np.argwhere(
+        (points[:, 0] == points[0, 0]) & (points[:, 2] == points[0, 2])
+    )  # x,z constant
+    indices_z = np.argwhere(
+        (points[:, 0] == points[0, 0]) & (points[:, 1] == points[0, 1])
+    )  # y,z constant
+    size_x = indices_x.shape[0]
+    size_y = indices_y.shape[0]
+    size_z = indices_z.shape[0]
+    points = points.reshape((size_x, size_y, size_z, 3))
+    data = data.reshape(
+        (time_steps_out.size, len(array_names), size_x, size_y, size_z)
+    )
+
+    return time_steps_out, points, data
